f27563841d9a36b1d6a0be33abf88e9e6d84b057
Joseph Akeni <josephakeni@gmail.com>
2022-03-30 19:41:51 +0100

    aws_ecs_service added

--- aws_projects/ecs/ecs-fargate.tf
+++ aws_projects/ecs/ecs-fargate.tf
@@ -34,3 +34,40 @@ resource "aws_ecs_task_definition" "main" {
     }]
   }])
 }
+
+resource "aws_ecs_service" "cluster" {
+  name            = "mycluster"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = 1
+  # iam_role        = aws_iam_role.foo.arn
+  # depends_on      = [aws_iam_role_policy.foo]
+
+  network_configuration {
+    subnets = [
+      "subnet-08e57b76de6f6ed39",
+      "subnet-05ce96e3ad611a35f",
+      "subnet-09957f0e1e8a4c570"
+    ]
+    security_groups = [
+      "sg-0d8c71d604117e188"
+    ]
+    assign_public_ip = "false"
+  }
+  
+  # ordered_placement_strategy {
+  #   type  = "binpack"
+  #   field = "cpu"
+  # }
+
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.foo.arn
+  #   container_name   = "mongo"
+  #   container_port   = 8080
+  # }
+
+  # placement_constraints {
+  #   type       = "memberOf"
+  #   expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  # }
+}
